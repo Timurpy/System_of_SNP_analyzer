20ebaff13d117031b08adb78a420e16a52cdf0f3
Alexey <lexage.korotkov@yandex.ru>
2023-10-11 18:15:53 +0300

    initial

---
+++ load_data.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import yaml
+
+def parse_from_config(config_path="config.yaml"):
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+    return config
+
+
+def process_df_columns_ang_get_patient_list(df):
+    columns = list(df.columns)
+    renamed_columns = []
+    unique_patients = []
+    for patient_name in columns:
+        if '(' in patient_name and ')' in patient_name:
+            patient_name = patient_name.replace('(', '_v').replace(')', '')
+        if '_v' in patient_name:
+            patient_name = patient_name.split('_v')
+            patient_id = int(patient_name[0].split()[-1])
+            patient_probe_number = patient_name[1].replace('.', '_')
+            new_name = f"patient_{patient_id}_{patient_probe_number}"
+            renamed_columns.append(new_name)
+            unique_patients.append(f"patient_{patient_id}")
+        else:
+            patient_id = int(patient_name.split()[-1])
+            patient_probe_number = 0
+            new_name = f"patient_{patient_id}_{patient_probe_number}"
+            renamed_columns.append(new_name)
+            unique_patients.append(f"patient_{patient_id}")
+
+    unique_patients = list(set(unique_patients))
+    df.columns = renamed_columns
+    return unique_patients
+
+def get_aggregated_patient_data_from_excel(config_path="config.yaml"):
+    config = parse_from_config(config_path)
+    
+    seq_threshhold_depth = config["seq_threshhold_depth"]
+    path_to_input_file = config["path_to_input_file"]
+    aggregation_method = config["aggregation_method"]
+
+    # loading input excel table
+    df = pd.read_excel(path_to_input_file, sheet_name=0)
+    # preprocessing dataframe, casting it into convinience format for further analysis 
+    df.replace({
+        'yes, ': '', 
+        'no': 0
+        }, 
+                regex=True, 
+                inplace=True
+                )
+    df.fillna(0, inplace=True)
+    df.index = list(map(lambda x: x.split()[-1], df['SNP']))
+    df.drop(['SNP'], axis=1, inplace=True)
+    df = df.astype(int)
+
+    unique_patients = process_df_columns_ang_get_patient_list(df)
+
+    for patient in unique_patients:
+        df_patient = df[[col for col in df.columns if f"{patient}_" in col]]
+        if aggregation_method == 'mean':
+            df[patient] = df_patient.mean(axis=1).astype(int)
+        elif aggregation_method == 'min':
+            df[patient] = df_patient.min(axis=1).astype(int)
+        elif aggregation_method == 'max':
+            df[patient] = df_patient.max(axis=1).astype(int)
+    
+    #thresholding
+    df[df < seq_threshhold_depth] = 0
+            
+    return df[unique_patients], unique_patients
---
+++ parse_data.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup as BS
+import requests
+
+def get_MAF_info_from_dbSNP(rs_id):
+    url = f"https://www.ncbi.nlm.nih.gov/snp/{rs_id}"
+    # Page request and HTML analysis using BeautifulSoup
+    with requests.Session() as s:
+        response = s.get(url)
+        soup = BS(response.text, "html.parser")
+        MAF_threshold = 0.3
+        
+        try: 
+            info = soup.find_all("dd")[4]
+        except IndexError:
+            return []
+        
+        info = str(info).split('\n')
+        info = [i.lstrip().replace('</div>', '').replace('<div>', '').replace('<div>', '') for i in info if (not '<span>' in i)]
+        info = [i for i in info if not i == '']
+        allele_data = []
+        i = 1
+        while i < len(info) - 1:
+            if '(' in info[i] and ')' in info[i]:
+                allele_data.append(info[i])
+                i += 1
+            else:
+                allele_data.append(f"{info[i]} {info[i+1]}")
+                i += 2
+        allele_data = sorted(allele_data, key=lambda x: float(x.split()[0].split('=')[-1]), reverse=True)
+        allele_data = [i for i in allele_data if float(i.split()[0].split('=')[-1]) >= MAF_threshold]
+        return allele_data
